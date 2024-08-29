ae81ae1cff1f9246ea4dee0619932700f0339c78
RinkCoder29 <vyacheslav.vereshchagin17@gmail.com>
2024-08-29 16:29:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+##task1
+print('1st program')
+
+a = (9**0.5)*5
+print(a)
+
+##task2
+print('2st program')
+
+a1 = 9.99 > 9.98
+b1 = 1000 != 1000.1
+print(a1 and b1)
+
+##task3
+print('3st program')
+
+a3 = 2*2+2
+b3 = 2*(2+2)
+print(a3==b3)
+
+##task4
+print('4st program')
+a4 = '123.456'
+a5 = ((float(a4)%1)*10)//1
+print(int(a5))
